5ad909581037e42f23b970b32ff74dc325a8d071
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-01-27 17:25:31 -0300

    FEAT: Select matches conclude | Create Matches concluded

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,16 +1,27 @@
+import TeamsModel from '../database/models/Teams';
 import Matches from '../database/models/Matches';
 
 class MatchesService {
   private _matches = Matches;
 
   public selectAllMatches = async () => {
-    const allMatches = await this._matches.findAll();
+    const allMatches = await this._matches.findAll({
+      attributes: { exclude: ['home_team_id', 'away_team_id'] },
+      include: [
+        { model: TeamsModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: TeamsModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
     return allMatches;
   };
 
   public selectAllInProgressMatches = async (query: string) => {
     const allInProgressMatches = await this._matches.findAll({
       where: { inProgress: query === 'true' },
+      include: [
+        { model: TeamsModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: TeamsModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
     });
     return allInProgressMatches;
   };
