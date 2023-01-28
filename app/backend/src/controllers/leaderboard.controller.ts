07130cbb1c516891636d45f6163104c128293990
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-01-28 18:58:53 -0300

    FEAT: Criação do arquivo de CONTROLLER para o LEADERBOARD

---
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import LeaderboardService from '../services/leaderboard.service';
+import statusCode from '../utils/status.code';
+
+class LeaderboardController {
+  constructor(private _leaderboardService = new LeaderboardService()) {}
+
+  public selectLeaderboardTable = async (_req: Request, res: Response): Promise<Response> => {
+    try {
+      const leaderboard = await this._leaderboardService.selectLeaderboardTable();
+      return res.status(statusCode.ok).json(leaderboard);
+    } catch (error: unknown) {
+      return res.status(statusCode.internalServerError).json({ message: error });
+    }
+  };
+}
+
+export default LeaderboardController;
