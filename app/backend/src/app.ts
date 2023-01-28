79ea8f120cac27dfa8369a85714fc412d0f1d954
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-01-28 17:40:10 -0300

    FEAT: Declarando nova rota LEADERBOARD no APP

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import loginRouter from './routes/login.router';
 import teamsRouter from './routes/teams.router';
 import matchesRouter from './routes/matches.router';
+import leaderboardRouter from './routes/leaderboard.router';
 
 class App {
   public app: express.Express;
@@ -32,6 +33,7 @@ class App {
     this.app.use('/login', loginRouter);
     this.app.use('/teams', teamsRouter);
     this.app.use('/matches', matchesRouter);
+    this.app.use('/leaderboard', leaderboardRouter);
   }
 
   public start(PORT: string | number):void {
